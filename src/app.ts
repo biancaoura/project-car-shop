96c3bf8f469a790e4ce29515775d6d1223c11ee2
Bianca Oura <biancaoura@gmail.com>
2023-02-25 18:00:10 -0300

    feat(motorcycles): /motorcycles route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import CarRoute from './Routes/Car';
+import MotorcycleRoute from './Routes/Motorcycle';
 import ErrorMiddleware from './Middlewares/ErrorMiddleware';
 
 const app = express();
@@ -7,6 +8,8 @@ const app = express();
 app.use(express.json());
 
 app.use('/cars', CarRoute);
+
+app.use('/motorcycles', MotorcycleRoute);
 
 app.use(ErrorMiddleware.error);
 
